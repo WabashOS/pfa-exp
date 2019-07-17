c633707e335941ea5c2285af91feea972ee63f82
Nathan Pemberton <nathanp@berkeley.edu>
2019-07-17 17:42:59 -0400

    Add better testing scripts to benchmarks

---
+++ benchmarks/runOne.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Usage:
+# ./runOne.sh args.cfg
+# see qsort.cfg for an example of args.cfg
+
+source $PWD/$1
+((m75 = (3*$workSz) / 4))
+((m50 = $workSz /2))
+((m25 = $workSz /4))
+
+echo "Testing: $CMD $ARGS"
+
+reset_cg $m75
+mytime pfa_launch $CMD $ARGS
+
+cat /sys/kernel/mm/pfa_stat_label
+cat /sys/kernel/mm/pfa_stat
