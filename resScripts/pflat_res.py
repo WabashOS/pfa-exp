ad4ee22808193b9729597fceb0d3eed1674c5847
Nathan Pemberton <nathanp@berkeley.edu>
2020-10-28 22:39:39 +0000

    pflat_res script works with firesim (some newline nonsense)

--- resScripts/pflat_res.py
+++ resScripts/pflat_res.py
@@ -13,6 +13,8 @@ outBase = Path(sys.argv[1])
 uartPath = next(outBase.glob('*/uartlog'))
 expName = str(uartPath.parts[-2])
 
+print("Uart Path: ")
+print(uartPath)
 class parseState(Enum):
     GET = auto()
     PRE_PFA = auto()
@@ -22,7 +24,7 @@ class parseState(Enum):
     LAT1 = auto()
     LAT2 = auto()
 
-with open(uartPath, 'r') as uartF:
+with open(uartPath, 'r', newline="\n") as uartF:
     getPat = re.compile(".*RMEM Get: ([-]?\d*) cycles")
 
     s = parseState.GET
@@ -30,6 +32,10 @@ with open(uartPath, 'r') as uartF:
     lat = ""
     tGet = None
     for line in uartF:
+        # FireSim spits out a bunch of \r\n (^M in vim) characters that mess
+        # everything up, clean them here (and in the "newlines=\n" think in
+        # open above)
+        line = line.replace("\r", "")
         if s == parseState.GET:
             getRes = getPat.match(line)
             if getRes:
@@ -39,28 +45,40 @@ with open(uartPath, 'r') as uartF:
             if line == "PFA STATS\n":
                 s = parseState.PFA1
         elif s == parseState.PFA1:
+            print("PFA1: ", repr(line))
             stat = line    
             s = parseState.PFA2
         elif s == parseState.PFA2:
+            print("PFA2 ", line)
             stat += line
             s = parseState.PRE_LAT
         elif s == parseState.PRE_LAT:
             if line == "PFLAT STAT\n":
                 s = parseState.LAT1
         elif s == parseState.LAT1:
+            print("lat1 ", line)
             lat = line
             s = parseState.LAT2
         elif s == parseState.LAT2:
+            print("lat2 ", line)
             lat += line
             break
 
-print("t_raw_read: ", tGet)
+print(stat)
+print(lat)
+if s != parseState.LAT2:
+    print("Could not parse uart output (" + str(uartPath) + ")")
+    sys.exit(1)
+
 statDF = pd.read_csv(StringIO(stat))
 latDF = pd.read_csv(StringIO(lat))
 finalDF = pd.concat([statDF[['t_rmem_read', 't_fault']], latDF], axis='columns')
 finalDF['t_rmem_read_raw'] = tGet
 
+print("Results:")
 print(finalDF)
+
+print("Writing results to: ", outBase / 'result.csv')
 with open(outBase / "result.csv", 'w') as f:
     finalDF.to_csv(f, index=False)
 
